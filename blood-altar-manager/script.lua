d824366f6725fb5f54e17df81b8b037bc69392d0
Jason Nash <jason@nashy.uk>
2023-09-03 20:54:37 +0100

    yet again order of operations, its hard

--- blood-altar-manager/script.lua
+++ blood-altar-manager/script.lua
@@ -1,4 +1,4 @@
-local component = require("component") 
+local component = require("component")
 local os = require("os")
 local term = require("term")
 
@@ -10,49 +10,57 @@ function check_target(transposer, target_side)
         if (info ~= nil) then
             term.write(info.label .. "\n")
             term.write(info.name .. "\n")
-        else 
+        else
             term.write("No target item detected" .. "\n")
         end
     end
     return info
 end
 
-function check_altar(transposer_fluids, transposer_items, altar_side_items, altar_side_fluids, stop_capacity, start_capacity, target_info)
+function check_altar(transposer_fluids, transposer_items, altar_side_items, altar_side_fluids, stop_capacity,
+    start_capacity, target_info)
     local capacity = transposer_fluids.getTankLevel(altar_side_fluids)
     local item_info = transposer_items.getStackInSlot(altar_side_items, 1)
 
-    if (item_info.name == target_info.name and item_info.label == target_info.label) then
-        if (settings.debug == true) then
-            term.write("Altar item match. \n")
-        end
-        return "match"
-    elseif (capacity < stop_capacity) then
+    -- Checks if altar has dropped below the shutdown threshold
+    if (capacity < stop_capacity) then
         if (settings.debug == true) then
             term.write("Altar bellow stop capacity. \n")
         end
         return "stop"
-    elseif (capacity < start_capacity) then
-        if (settings.debug == true) then
-            term.write("Altar bellow start capacity. \n")
-        end
-        return "low"
-    elseif (item_info == nil) then
+    end
+
+    -- Checks to see if altar is empty, 
+    -- if the altar is empty then return that it is empty.
+    -- if the altar is NOT empty then check if the altars item matches the target item
+    if (item_info == nil) then
         if (settings.debug == true) then
             term.write("Altar empty. \n")
         end
         return "empty"
     else
-        if (settings.debug == true) then
-            term.write("Alter contains non target item. \n")
+        if (item_info.name == target_info.name and item_info.label == target_info.label) then
+            if (settings.debug == true) then
+                term.write("Altar item match. \n")
+            end
+            return "match"
         end
-        return "invalid"
     end
+
+    -- Checks if the altar has enough blood to start a run
+    if (capacity < start_capacity) then
+        if (settings.debug == true) then
+            term.write("Altar bellow start capacity. \n")
+        end
+        return "low"
+    end
+
 end
 
 function altar_extract(transposer, altar_side, output_side)
     local item_info = transposer.getStackInSlot(altar_side, 1)
     while item_info ~= nil do
-        for slot=1, transposer.getInventorySize(output_side), 1 do
+        for slot = 1, transposer.getInventorySize(output_side), 1 do
             local chest_item = transposer.getStackInSlot(output_side, slot)
             if (chest_item == nil) then
                 transposer.transferItem(altar_side, output_side, 1, 1, slot)
@@ -69,7 +77,7 @@ function altar_extract(transposer, altar_side, output_side)
 end
 
 function altar_insert(transposer, altar_side, input_side, transfer_count)
-    for slot=1, transposer.getInventorySize(input_side), 1 do
+    for slot = 1, transposer.getInventorySize(input_side), 1 do
         local chest_item = transposer.getStackInSlot(input_side, slot)
         if (chest_item ~= nil) then
             transposer.transferItem(input_side, altar_side, transfer_count, slot, 1)
@@ -83,7 +91,7 @@ while true do
     if (settings.debug == true) then
         term.clear()
     end
-    
+
     for index in pairs(settings.altars) do
         local transposer_fluids = component.proxy(component.get(settings.altars[index].transposer_fluid_address))
         local transposer_items = component.proxy(component.get(settings.altars[index].transposer_item_address))
@@ -99,13 +107,14 @@ while true do
         local start_capacity = settings.altars[index].altar_start_capacity
         local stop_capacity = settings.altars[index].altar_stop_capacity
         local using_buffer = settings.altars[index].using_buffer
-    
+
         if (settings.debug == true) then
             term.write(transposer_target.address .. "\n")
         end
-        local target_info = check_target(transposer_target, target_side) 
+        local target_info = check_target(transposer_target, target_side)
         if (target_info ~= null) then
-            local altar_info = check_altar(transposer_fluids, transposer_items, altar_side_items, altar_side_fluids, stop_capacity, start_capacity, target_info)
+            local altar_info = check_altar(transposer_fluids, transposer_items, altar_side_items, altar_side_fluids,
+                stop_capacity, start_capacity, target_info)
             if (altar_info == "stop" and transposer_items.getStackInSlot(altar_side_items, 1) ~= nil) then
                 altar_extract(transposer_items, altar_side_items, output_side)
             elseif (altar_info == "match") then
@@ -114,7 +123,7 @@ while true do
                 local insert = altar_insert(transposer_items, altar_side_items, input_side, transfer_count)
             end
         elseif (using_buffer == true and transposer_items.getStackInSlot(input_side, 1) ~= nil) then
-            transposer_items.transferItem(input_side, output_side, transposer_items.getSlotStackSize(input_side, 1),  1)
+            transposer_items.transferItem(input_side, output_side, transposer_items.getSlotStackSize(input_side, 1), 1)
         end
     end
     os.sleep(3)
