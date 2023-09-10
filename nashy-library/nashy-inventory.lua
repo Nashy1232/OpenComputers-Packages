4536c2b4bef235594ec68cb74d8cad68fc231a5b
Jason Nash <jason@nashy.uk>
2023-09-10 01:49:10 +0100

    update

--- nashy-library/nashy-inventory.lua
+++ nashy-library/nashy-inventory.lua
@@ -7,9 +7,12 @@ local inventory = {}
 
 -- check to see if inventory contains any items
 function inventory.isEmpty(transposer, side)
+    if (transposer == nil or side == nil) then
+        return nil
+    end
+
     local slots = transposer.getInventorySize(side)
-    term.write(tostring(slots))
-    for index in pairs(slots) do
+    for index = 1, slots, 1 do
         local stack = transposer.getStackInSlot()
         if (stack ~= nil and stack.size > 0) then
             return false
