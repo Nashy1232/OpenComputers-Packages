fc68957d45a6e6ad779c9fc17480811bcc77b4b0
Jason Nash <jason@nashy.uk>
2023-09-11 15:09:19 +0100

    update

--- blood-altar-manager/script.lua
+++ blood-altar-manager/script.lua
@@ -70,13 +70,13 @@ function altar_extract(transposer, altar_side, output_side)
     while item_info ~= nil do
         for slot = 1, transposer.getInventorySize(output_side), 1 do
             local inv_item = transposer.getStackInSlot(output_side, slot)
+            local inv_item_stack = transposer.getSlotStackSize(output_side, slot) or 0
+            local inv_item_max_stack = transposer.getSlotMaxStackSize(output_side, slot) or 0
             if (inv_item == nil or inv_item == nil) then
                 transposer.transferItem(altar_side, output_side, 1, 1, slot)
                 break
-            elseif (item_info.label == inv_item.label) then
-                local inv_item_stack = inv_item.getSlotStackSize(output_side, slot)
-                local inv_item_max_stack = inv_item.getSlotMaxStackSize(output_side, slot)
-                term.write(tostring(inv_item_max_stack))
+            elseif (item_info.label == inv_item.label and inv_item_stack < inv_item_max_stack) then
+                transposer.transferItem(altar_side, output_side, 1, 1, slot)
                 break
             end
             os.sleep(0.1)
