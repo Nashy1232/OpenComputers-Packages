6d4c01c50e992cf07bde4809b5c9844fa53edaa3
Jason Nash <jason@nashy.uk>
2023-09-13 21:12:18 +0100

    update

--- diesel-engine-manager/script.lua
+++ diesel-engine-manager/script.lua
@@ -4,13 +4,6 @@ local term = require("term")
 
 local settings = dofile("/usr/bin/diesel-engine-manager/settings.cfg")
 
-local function rs(address, val)
-    for address in pairs(settings.redstone_addresses) do
-        local rs = component.proxy(component.get(address))
-        rs.setOutput({val, val, val, val, val, val})
-    end
-end
-
 while true do
     if (settings.debug == true) then
         term.clear()
@@ -19,16 +12,16 @@ while true do
     for index in pairs(settings.generators) do
         os.sleep(1)
         local battery_buffer = component.proxy(component.get(settings.generators[index].battery_buffer_address))
-        local redstone_address = component.proxy(component.get(settings.generators[index].redstone_address))
+        local redstone = component.proxy(component.get(settings.generators[index].redstone_address))
         local low_capacity = settings.generators[index].low_capacity
         local high_capacity = settings.generators[index].high_capacity
         local battery_percent = battery_buffer.getEnergyStored() / battery_buffer.getEnergyCapacity()
         local enabled = settings.generators[index].enabled
 
         if (battery_percent > high_capacity) then
-            rs(redstone_address, 0)
+            redstone.setOutput(0)
         elseif (battery_percent < low_capacity and enabled) then
-            rs(redstone_address, 15)
+            redstone.setOutput(15)
         end
     end
     os.sleep(1)
