4df8347b423f35776808bf9b01a9c466afb6085e
Jason Nash <jason@nashy.uk>
2025-02-16 15:34:50 +0000

    oops

--- template/script.lua
+++ template/script.lua
@@ -1,20 +1,5 @@
-local event = require "event"
-local shell = require "shell"
-local thread = require "thread"
+local os = require("os")
+local component = require("component") 
+local term = require("term")
 
-local threads = {}
-local program = "/usr/bin/template/script.lua"
-
-function start()
-    local proc = thread.create(shell.execute, program, os.getenv("shell"))
-    proc:detach()
-    table.insert(threads, proc)
-end
-
-function stop()
-    for i, proc in  ipairs(threads) do
-        if proc.status and proc:status() ~= "dead" then
-            proc:kill()
-        end
-    end
-end
+local settings = dofile("/usr/bin/celestial-crystal-cluster/settings.cfg")
