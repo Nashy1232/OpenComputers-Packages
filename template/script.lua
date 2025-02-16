61f0f04601b8184e165bba4eb62500bd5da387a7
Jason Nash <jason@nashy.uk>
2025-02-16 15:35:34 +0000

    update

--- template/script.lua
+++ template/script.lua
@@ -2,4 +2,4 @@ local os = require("os")
 local component = require("component") 
 local term = require("term")
 
-local settings = dofile("/usr/bin/celestial-crystal-cluster/settings.cfg")
+local settings = dofile("/usr/bin/template/settings.cfg")
