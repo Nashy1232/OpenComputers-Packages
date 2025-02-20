27ddf8bb795cacc896263013b111d679dd04f51f
Carling2u <carling2u@hotmail.com>
2025-02-20 23:40:42 +0000

    a

--- test-carling/script.lua
+++ test-carling/script.lua
@@ -4,3 +4,8 @@ local term = require("term")
 
 local settings = dofile("/usr/bin/test-carling/settings.cfg")
 
+component.proxy(component.get(settings.stage1.address)).setOutput(settings.stage1.side, settings.output)
+component.proxy(component.get(settings.stage2.address)).setOutput(settings.stage2.side, settings.output)
+component.proxy(component.get(settings.stage3.address)).setOutput(settings.stage3.side, settings.output)
+component.proxy(component.get(settings.stage4.address)).setOutput(settings.stage4.side, settings.output)
+component.proxy(component.get(settings.activate.address)).setOutput(settings.activate.side, settings.output)
