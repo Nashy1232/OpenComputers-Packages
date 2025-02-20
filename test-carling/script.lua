1da58e2f73676a319ef392c0a5293519910dc360
Carling2u <carling2u@hotmail.com>
2025-02-20 22:40:46 +0000

    Update script.lua

--- test-carling/script.lua
+++ test-carling/script.lua
@@ -2,4 +2,5 @@ local os = require("os")
 local component = require("component") 
 local term = require("term")
 
-local settings = dofile("/usr/bin/template/settings.cfg")
+local settings = dofile("/usr/bin/test-carling/settings.cfg")
+
