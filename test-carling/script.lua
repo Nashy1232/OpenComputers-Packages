cbc90c75af2c02ffe8972cdc2b055dafbe55efce
Jason Nash <jason@nashy.uk>
2025-02-20 23:51:18 +0000

    update

--- test-carling/script.lua
+++ test-carling/script.lua
@@ -4,8 +4,8 @@ local term = require("term")
 
 local settings = dofile("/usr/bin/test-carling/settings.cfg")
 
-component.proxy(component.get(settings.stage1.address)).setOutput(settings.stage1.side, settings.output)
-component.proxy(component.get(settings.stage2.address)).setOutput(settings.stage2.side, settings.output)
-component.proxy(component.get(settings.stage3.address)).setOutput(settings.stage3.side, settings.output)
-component.proxy(component.get(settings.stage4.address)).setOutput(settings.stage4.side, settings.output)
-component.proxy(component.get(settings.activate.address)).setOutput(settings.activate.side, settings.output)
+component.proxy(component.get(settings.stage1.id)).setOutput(settings.stage1.side, settings.output)
+component.proxy(component.get(settings.stage2.id)).setOutput(settings.stage2.side, settings.output)
+component.proxy(component.get(settings.stage3.id)).setOutput(settings.stage3.side, settings.output)
+component.proxy(component.get(settings.stage4.id)).setOutput(settings.stage4.side, settings.output)
+component.proxy(component.get(settings.activate.id)).setOutput(settings.activate.side, settings.output)
