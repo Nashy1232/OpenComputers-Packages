5c568fb50e21b5acad9b3bcdce0d18f39cb9cffa
Carling2u <carling2u@hotmail.com>
2025-02-21 01:46:17 +0000

    f

--- test-carling/script.lua
+++ test-carling/script.lua
@@ -8,12 +8,15 @@ function setState(obj, value)
     component.proxy(component.get(obj.id)).setOutput(obj.side, value)
 end
 
-while true do
+function clear()
     setState(settings.stage1, 0)
     setState(settings.stage2, 0)
     setState(settings.stage3, 0)
     setState(settings.stage4, 0)
     setState(settings.activate, 0)
+end
+
+function runSequence()
     os.sleep(1)
     setState(settings.stage1, 15)
     os.sleep(1)
@@ -24,5 +27,21 @@ while true do
     setState(settings.stage4, 15)
     os.sleep(1)
     setState(settings.activate, 15)
-    os.sleep(5)
 end
+
+function getItems(transposer)
+    local item = component.proxy(component.get(settings.transposer.id))
+    local slots = item.getInventorySize(settings.transposer.side)
+
+    term.write("Slots: " .. slots .. "\n")
+
+    for i=1, slots, 1 do
+        --getSlotStackSize(side:number, slot:number)
+        local count = item.getSlotStackSize(transposer.side, i)
+        term.write(i .. ": " .. count .. "\n")
+    end
+end
+
+term.write("Running...\n")
+getItems(settings.transposer)
+term.write("Program Complete.")
