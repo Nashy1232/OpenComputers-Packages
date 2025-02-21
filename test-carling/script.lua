e3750fb4df92c9a54db4174ce54c4c69203e50ee
Carling2u <carling2u@hotmail.com>
2025-02-21 13:25:46 +0000

    Refactor

--- test-carling/script.lua
+++ test-carling/script.lua
@@ -5,46 +5,39 @@ local inventory = require("nashy-inventory")
 
 local settings = dofile("/usr/bin/test-carling/settings.cfg")
 
-function setState(obj, value)
-    component.proxy(component.get(obj.id)).setOutput(obj.side, value)
+function getBlock(id)
+    return component.proxy(component.get(id))
 end
 
-function clear()
-    setState(settings.stage1, 0)
-    setState(settings.stage2, 0)
-    setState(settings.stage3, 0)
-    setState(settings.stage4, 0)
-    setState(settings.activate, 0)
+function clearStages()
+    for i, stage in ipairs(settings.stages) do
+        getBlock(stage.id).setOutput(stage.side, 0)
+    end
 end
 
-function runSequence()
-    clear()
-    os.sleep(1)
-    setState(settings.stage1, 15)
-    os.sleep(1)
-    setState(settings.stage2, 15)
-    os.sleep(1)
-    setState(settings.stage3, 15)
-    os.sleep(1)
-    setState(settings.stage4, 15)
-    os.sleep(1)
-    setState(settings.activate, 15)
-    os.sleep(2)
-    clear()
+function runStages()
+    for i, stage in ipairs(settings.stages) do
+        getBlock(stage.id).setOutput(stage.side, 15)
+        os.sleep(stage.delay)
+    end
 end
 
 term.write("Running...\n")
+clearStages()
+os.sleep(1)
+runStages()
+term.write("Complete.\n")
 
-while true do
-    local transposer = component.proxy(component.get(settings.transposer.id))
-    local empty = inventory.isEmpty(transposer, settings.transposer.side)
+-- while true do
+--     local transposer = component.proxy(component.get(settings.transposer.id))
+--     local empty = inventory.isEmpty(transposer, settings.transposer.side)
 
-    if (not empty) then
-        term.write("Running Sequence\n")
-        runSequence()
-    else 
-        term.write("Loop\n")
-    end
+--     if (not empty) then
+--         term.write("Running Sequence\n")
+--         runSequence()
+--     else 
+--         term.write("Loop\n")
+--     end
 
-    os.sleep(3)
-end
+--     os.sleep(3)
+-- end
